077b69b02e991c1359465775f780064e500e589e
Andy Elster <andy.elster@gmail.com>
2018-01-28 00:33:54 -0800

    Working with basic PDO functionality PDO for User calls/adds/updates not yet working

--- php/Common/DoQuery.php
+++ php/Common/DoQuery.php
@@ -1,51 +1,49 @@
 <?php
 
-global $mysql_last_id;
-global $mysql_numrows;
-global $mysql_result;
+function DoQuery($query) {
+    $debug = $GLOBALS['gDebug'];
 
-function DoQuery()
-{
-	$num_args = func_num_args();
-	$query = func_get_arg( 0 );
-	$db = ( $num_args == 1 ) ? $GLOBALS[ 'mysql_db' ] : func_get_arg( 1 );
-	$last_id = 0;	
+    if ($debug)
+        $dmsg = "&nbsp;&nbsp;&nbsp;&nbsp;DoQuery: $query";
 
-	$debug = $GLOBALS[ 'gDebug' ];
-	$support = $GLOBALS['mysql_admin'];
-	
-	if( $debug ) $dmsg = "&nbsp;&nbsp;&nbsp;&nbsp;DoQuery: $query";
-	
-	$result = mysql_query( $query, $db );
-	if( mysql_errno( $db ) != 0 )
-	{
-		if( ! $db ) { echo "  query: $query<br>\n"; }
-		echo "  result: " . mysql_error( $db ) . "<br>\n";
-		echo "I'm sorry but something unexpected occurred.  Please send all details<br>";
-		echo "of what you were doing and any error messages to $support<br>";
-	}
-	else
-	{
-		if( preg_match( "/^select/i", $query ) )
-		{
-			$numrows = mysql_num_rows( $result );
-		}
-		elseif( preg_match( "/^insert/i", $query ) )
-		{
-			$numrows = mysql_affected_rows( $db );
-			$last_id = mysql_insert_id();
-		}
-		else
-		{
-			$numrows = mysql_affected_rows( $db );
-		}
-		if( $debug ) $dmsg .= sprintf( ", # rows: %d", $numrows );
-	}
-	
- 	if( $debug ) Logger( $dmsg );
- 
-	$GLOBALS[ 'mysql_last_id' ] = $last_id;
-	$GLOBALS[ 'mysql_numrows' ] = $numrows;
-	$GLOBALS[ 'mysql_result' ] = $result;
+    $stmt = $GLOBALS['gDb']->prepare($query);
+    try {
+        if (func_num_args() == 1) {
+            $stmt->execute();
+        } else {
+            $args = func_get_arg(1);
+            $stmt->execute($args);
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        echo '<pre>' . $e->getTraceAsString() . '</pre>';
+    }
+    $GLOBALS['gPDO_num_rows'] = $stmt->rowCount();
+    /*
+      if (mysql_errno($db) != 0) {
+      if (!$db) {
+      echo "  query: $query<br>\n";
+      }
+      echo "  result: " . mysql_error($db) . "<br>\n";
+      echo "I'm sorry but something unexpected occurred.  Please send all details<br>";
+      echo "of what you were doing and any error messages to $support<br>";
+      } else {
+      if (preg_match("/^select/i", $query)) {
+      $numrows = mysql_num_rows($result);
+      } elseif (preg_match("/^insert/i", $query)) {
+      $numrows = mysql_affected_rows($db);
+      $last_id = mysql_insert_id();
+      } else {
+      $numrows = mysql_affected_rows($db);
+      }
+     * 
+     */
+    if ($debug)
+        $dmsg .= sprintf(", # rows: %d", $GLOBALS['gPDO_num_rows']);
+
+
+    if ($debug)
+        Logger($dmsg);
+    
+    return $stmt;
 }
-?>
