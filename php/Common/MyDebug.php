eb1ff0eaac06248682a3334d0c73269ba918e086
Andy Elster <andy.elster@gmail.com>
2019-11-03 11:13:33 -0800

    Setup for Dreamweaver

--- php/Common/MyDebug.php
+++ php/Common/MyDebug.php
@@ -5,34 +5,40 @@ function MyDebug() {
         $GLOBALS['gFunction'][] = __FUNCTION__;
         Logger();
     }
-    $gUserId = $GLOBALS['gUserId'];
-    $gFunc = $GLOBALS['gFunc'];
-    $gDebug = $GLOBALS['gDebug'];
-    $gDebugAll = $GLOBALS['gDebugAll'];
+    $userId = $GLOBALS['gUserId'];
+    $func = $GLOBALS['gFunc'];
+    $debug = $GLOBALS['gDebug'];
+    
     $gDebugInLine = $GLOBALS['gDebugInLine'];
     $gDebugErrorLog = $GLOBALS['gDebugErrorLog'];
     $gDebugWindow = $GLOBALS['gDebugWindow'];
     $gDebugHTML = $GLOBALS['gDebugHTML'];
+    $gDebugAll = $GLOBALS['gDebugAll'];
+
+    $dreamweaver = array_key_exists('gDreamweaver',$GLOBALS) ? $GLOBALS['gDreamweaver'] : 0;
     
-    if( $gFunc == 'display' ) {
-        echo "<div class=center>";
-        echo "<h2>Debug Control</h2>";
+    $save_db = $GLOBALS['gDb'];
+    $GLOBALS['gDb'] = $GLOBALS['gDbAll'][$GLOBALS['gDbControlId']];
         
-        echo "<input type=button value=Back onclick=\"setValue('from', 'MyDebug');addAction('Back');\">";
-        
-/*        $tag = MakeTag('update');
-        $jsx = array();
-        $jsx[] = "setValue('area','debug')";
-        $jsx[] = "setValue('from','MyDebug')";
-        $jsx[] = "setValue('func','update')";
-        $jsx[] = "addAction('Update')";
-        $js = sprintf("onClick=\"%s\"", join(';', $jsx));
-        echo "<input type=button value=Update $tag $js>";
-*/
-        echo "<br><br>";
-        
-        echo "</div>";
-        
+    if ($func == 'display') {
+        if ( ! $dreamweaver) {
+            echo "<div class=center>";
+            echo "<h2>Debug Control</h2>";
+
+            echo "<input type=button value=Back onclick=\"setValue('from', 'MyDebug');addAction('Back');\">";
+            echo "<br><br>";
+
+            echo "</div>";
+        }
+        /*        $tag = MakeTag('update');
+          $jsx = array();
+          $jsx[] = "setValue('area','debug')";
+          $jsx[] = "setValue('from','MyDebug')";
+          $jsx[] = "setValue('func','update')";
+          $jsx[] = "addAction('Update')";
+          $js = sprintf("onClick=\"%s\"", join(';', $jsx));
+          echo "<input type=button value=Update $tag $js>";
+         */
         echo "<table class=debug>";
         echo "<tr>";
         echo "  <th>Option</th>";
@@ -43,61 +49,63 @@ function MyDebug() {
         $fld = 'DebugAll';
         echo "<tr>";
         echo "  <td>All Debug</td>";
-        
+
         $options = array();
-        if( $gDebug == 0 ) {
+        if ($debug == 0) {
             $state = "All Off";
-            $options[] = [ 'label' => '-- select --', 'value' => -1 ];
-            $options[] = [ 'label' => 'All On', 'value' => $gDebugAll ];
+            $options[] = ['label' => '-- select --', 'value' => -1];
+            $options[] = ['label' => 'All On', 'value' => $gDebugAll];
             $class = 'dbg-off';
-        } elseif ($gDebug == $GLOBALS['gDebugAll'] ) {
+        } elseif ($debug == $gDebugAll) {
             $state = "All On";
-            $options[] = [ 'label' => '-- select --', 'value' => -1 ];
-            $options[] = [ 'label' => 'All Off', 'value' => 0 ];
+            $options[] = ['label' => '-- select --', 'value' => -1];
+            $options[] = ['label' => 'All Off', 'value' => 0];
             $class = 'dbg-on';
         } else {
-            $state = "Part On";                        
-            $options[] = [ 'label' => '-- select --', 'value' => -1 ];
-            $options[] = [ 'label' => 'All On', 'value' => $gDebugAll ];
-            $options[] = [ 'label' => 'All Off', 'value' => 0 ];
+            $state = "Part On";
+            $options[] = ['label' => '-- select --', 'value' => -1];
+            $options[] = ['label' => 'All On', 'value' => $gDebugAll];
+            $options[] = ['label' => 'All Off', 'value' => 0];
             $class = 'dbg-part';
         }
         echo "<td class='$class'>$state</td>";
-        
+
         $jsx = array();
         $jsx[] = "setValue('from','MyDebug')";
         $jsx[] = "addField('$fld')";
+        $jsx[] = "setValue('mode','control')";
         $jsx[] = "setValue('area','debug')";
-        $jsx[] = "setValue('from','MyDebug')";
+        $jsx[] = "setValue('from','" . __FUNCTION__ . "')";
         $jsx[] = "setValue('func','update')";
         $jsx[] = "addAction('Update')";
         $js = sprintf("onChange=\"%s\"", join(';', $jsx));
         $tag = MakeTag("$fld");
         echo "<td>";
         echo "<select $tag $js>";
-        foreach( $options as $row ) {
-            printf( "<option value=%d>%s</option>", $row['value'], $row['label']);
+        foreach ($options as $row) {
+            printf("<option value=%d>%s</option>", $row['value'], $row['label']);
         }
         echo "</select>";
         echo "</td>";
         echo "</tr>";
 #==================================================================
         $fld = 'DebugInLine';
-        
+
         echo "<tr>";
         echo "  <td>In Line</td>";
 
         $jsx = array();
         $jsx[] = "setValue('from','MyDebug')";
         $jsx[] = "addField('$fld')";
+        $jsx[] = "setValue('mode','control')";
         $jsx[] = "setValue('area','debug')";
-        $jsx[] = "setValue('from','MyDebug')";
+        $jsx[] = "setValue('from','" . __FUNCTION__ . "')";
         $jsx[] = "setValue('func','update')";
         $jsx[] = "addAction('Update')";
         $js = sprintf("onChange=\"%s\"", join(';', $jsx));
         $tag = MakeTag("$fld");
-        $enabled = ($gDebug & $gDebugInLine) ? 1 : 0;
-        if( $enabled ) {
+        $enabled = ($debug & $gDebugInLine) ? 1 : 0;
+        if ($enabled) {
             echo "<td class=dbg-on>On</td>";
             echo "<td><select $tag $js>";
             echo "<option value=-1>-- select --</option>";
@@ -111,26 +119,27 @@ function MyDebug() {
         echo "</select>";
         echo "</td>";
         echo "</tr>";
-        
+
 #==================================================================
         $fld = 'DebugErrorLog';
-        
+
         echo "<tr>";
         echo "  <td>Error Log</td>";
 
         $jsx = array();
         $jsx[] = "setValue('from','MyDebug')";
         $jsx[] = "addField('$fld')";
+        $jsx[] = "setValue('mode','control')";
         $jsx[] = "setValue('area','debug')";
-        $jsx[] = "setValue('from','MyDebug')";
+        $jsx[] = "setValue('from','" . __FUNCTION__ . "')";
         $jsx[] = "setValue('func','update')";
         $jsx[] = "addAction('Update')";
         $js = sprintf("onChange=\"%s\"", join(';', $jsx));
         $tag = MakeTag("$fld");
-        
-        $enabled = ($gDebug & $gDebugErrorLog) ? 1 : 0;
 
-        if( $enabled ) {
+        $enabled = ($debug & $gDebugErrorLog) ? 1 : 0;
+
+        if ($enabled) {
             echo "<td class=dbg-on>On</td>";
             echo "<td><select $tag $js>";
             echo "<option value=-1>-- select --</option>";
@@ -147,22 +156,23 @@ function MyDebug() {
 
 #==================================================================
         $fld = 'DebugWindow';
-        
+
         echo "<tr>";
         echo "  <td>Window</td>";
 
         $jsx = array();
         $jsx[] = "setValue('from','MyDebug')";
         $jsx[] = "addField('$fld')";
+        $jsx[] = "setValue('mode','control')";
         $jsx[] = "setValue('area','debug')";
-        $jsx[] = "setValue('from','MyDebug')";
+        $jsx[] = "setValue('from','" . __FUNCTION__ . "')";
         $jsx[] = "setValue('func','update')";
         $jsx[] = "addAction('Update')";
         $js = sprintf("onChange=\"%s\"", join(';', $jsx));
         $tag = MakeTag("$fld");
-        
-        $enabled = ($gDebug & $gDebugWindow) ? 1 : 0;
-        if( $enabled ) {
+
+        $enabled = ($debug & $gDebugWindow) ? 1 : 0;
+        if ($enabled) {
             echo "<td class=dbg-on>On</td>";
             echo "<td><select $tag $js>";
             echo "<option value=-1>-- select --</option>";
@@ -176,25 +186,26 @@ function MyDebug() {
         echo "</select>";
         echo "</td>";
         echo "</tr>";
-        
+
 #==================================================================
         $fld = 'DebugHTML';
-        
+
         echo "<tr>";
         echo "  <td>HTML</td>";
 
         $jsx = array();
         $jsx[] = "setValue('from','MyDebug')";
         $jsx[] = "addField('$fld')";
+        $jsx[] = "setValue('mode','control')";
         $jsx[] = "setValue('area','debug')";
-        $jsx[] = "setValue('from','MyDebug')";
+        $jsx[] = "setValue('from','" . __FUNCTION__ . "')";
         $jsx[] = "setValue('func','update')";
         $jsx[] = "addAction('Update')";
         $js = sprintf("onChange=\"%s\"", join(';', $jsx));
         $tag = MakeTag("$fld");
-        
-        $enabled = ($gDebug & $gDebugHTML) ? 1 : 0;
-        if( $enabled ) {
+
+        $enabled = ($debug & $gDebugHTML) ? 1 : 0;
+        if ($enabled) {
             echo "<td class=dbg-on>On</td>";
             echo "<td><select $tag $js>";
             echo "<option value=-1>-- select --</option>";
@@ -208,34 +219,34 @@ function MyDebug() {
         echo "</select>";
         echo "</td>";
         echo "</tr>";
-        
+
         echo "</table>";
-        
-    } elseif( $gFunc == 'update' ) {
+    } elseif ($func == 'update') {
         $tmp2 = preg_split('/,/', $_POST['fields'], NULL, PREG_SPLIT_NO_EMPTY);
         $tmp = array_unique($tmp2);
-        $debug = $gDebug;
-        foreach( $tmp as $fld ) {
+        foreach ($tmp as $fld) {
             $var = "g$fld";
-            if( $fld == 'DebugAll' ) {
+            if ($fld == 'DebugAll') {
                 $debug = $_POST['DebugAll'];
             } else {
-                if( $_POST[$fld] == 0 ) {
-                    $debug -= $$var;
-                    if( $fld == 'DebugWindow' ) {
-                        echo "<script type='text/javascript'>closeDebugWindow();</script>";
+                if ($_POST[$fld] == 0) {
+                    if( ($debug & $$var ) > 0 ) {
+                        $debug -= $$var;
+                        if ($fld == 'DebugWindow') {
+                            echo "<script type='text/javascript'>closeDebugWindow();</script>";
+                        }
                     }
-                } elseif( $_POST[$fld] == 1 ) {
-                    $debug += $$var;
+                } elseif ($_POST[$fld] == 1) {
+                    $debug = $debug | $$var;
                 }
             }
+            error_log( "** var: [$var], debug: [$debug] **" );
         }
-        $GLOBALS['gDb'] = $GLOBALS['gDbVector'][0];  
 
-        $query = "update users set debug = :v1 where userid = :v2";        
-        DoQuery( $query, [':v1' => $debug, ':v2' => $gUserId ] );
-        
-        $GLOBALS['gDb'] = $GLOBALS['gDbVector'][$_SESSION['dbId']];
+        $query = "update users set debug = :v1 where userid = :v2";
+        DoQuery($query, [':v1' => $debug, ':v2' => $userId]);
+
+        $GLOBALS['gDb'] = $save_db;
 
         $GLOBALS['gDebug'] = $debug;
     }
